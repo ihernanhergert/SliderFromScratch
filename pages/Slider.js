d221b5a70a3480e126f578e723a81d8c69eb7eb9
unknown <ihernanhergert@gmail.com>
2022-05-26 23:18:43 -0300

    first commit

---
+++ pages/Slider.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import arrowRight from "../public/rightArrow.svg";
+import Image from "next/image";
+import { SliderData } from "./SliderData";
+import arrowLeft from '../public/leftArrow.svg'
+
+
+const Slider = ({ slides }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("Intervalo");
+      nextSlide();
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [current]);
+  
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
+  return (
+    <section className="Slider">
+      <div className="arrows">
+        <div className="arrowLeft" onClick={prevSlide}>
+          <Image src={arrowLeft} height={"50px"} width={"50px"}/>
+        </div>
+
+        <div className="arrowRight" onClick={nextSlide}>
+          <Image src={arrowRight} height={"50px"} width={"50px"} />
+        </div>
+      </div>
+      {SliderData.map((slide, index) => {
+        return (
+          <div
+            className={index === current ? "slide active" : "slide"}
+            key={index}
+          >
+            {index === current && (
+              <>
+                <img
+                  src={slide.image}
+                  alt="afsjfh"
+                  index={index}
+                  className="image"
+                />
+                <h2>{slide.title}</h2>
+                <p>{slide.desc}</p>
+                <button><a href={slide.link} target="_blank" rel="noopener noreferrer">Ver Oferta</a></button>
+              </>
+            )}
+          </div>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Slider;
---
+++ pages/SliderData.js
@@ -0,0 +1,37 @@
+export const SliderData = [
+  {
+    id: 1,
+    title: "Metaverse",
+    image: "/1.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  ",
+    link: "https://www.youtube.com",
+  },
+  {
+    id: 2,
+    title: "Placas De video",
+    image: "/2.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  ",
+    link: "https://www.facebook.com",
+  },
+  {
+    id: 3,
+    title: "Memorias RAM DDR5",
+    image: "/3.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  ",
+    link: "https://www.twitter.com",
+  },
+  {
+    id: 4,
+    title: "Promos Pc Gamer",
+    image: "/4.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  ",
+    link: "/4.jpg",
+  },
+  {
+    id: 5,
+    title: "AMD Rizen 9",
+    image: "/5.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  ",
+    link: "/5.jpg",
+  },
+];
